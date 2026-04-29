af2be27dc4ad57c425308e9da4c9eeb02fb5b1fc
Piyush Goenka <goenkapiyush2005@gmail.com>
2026-04-29 13:48:46 +0530

    exported database

--- scripts/export-database.ts
+++ scripts/export-database.ts
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const { getMongoDb } = require('../lib/mongodb');
+import fs from 'fs';
+import path from 'path';
+import { getMongoDb } from '../lib/mongodb';
 
 async function exportDatabase() {
   try {
